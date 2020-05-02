67f3804f2aed3378fadf3d5d0aab4ce4621044dc
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-05-02 18:29:18 +0530

    Allow specifying a crop for the videos

--- scripts/split-video.py
+++ scripts/split-video.py
@@ -10,7 +10,7 @@ def to_seconds(timestamp):
     return times[0] * 60 + times[1]
 
 
-def main(video_path, timings, n):
+def main(video_path, timings, n, crop=None):
     video_name = os.path.basename(video_path)
     for i, line in enumerate(timings, start=1):
         if n and i != n:
@@ -18,21 +18,23 @@ def main(video_path, timings, n):
         start, end = line.split("-")
         start_seconds = to_seconds(start)
         end_seconds = to_seconds(end)
-        print(start_seconds, end_seconds)
         duration = end_seconds - start_seconds
+        command = [
+            "ffmpeg",
+            "-y",
+            "-i",
+            video_name,
+            "-ss",
+            str(start_seconds),
+            "-t",
+            str(duration),
+            f"part-{i:02d}-{video_name}",
+        ]
+        if crop:
+            command.insert(-1, "-filter:v")
+            command.insert(-1, f"crop={crop}")
         subprocess.call(
-            [
-                "ffmpeg",
-                "-y",
-                "-i",
-                video_name,
-                "-ss",
-                str(start_seconds),
-                "-t",
-                str(duration),
-                f"part-{i:02d}-{video_name}",
-            ],
-            cwd=os.path.dirname(video_path),
+            command, cwd=os.path.dirname(video_path),
         )
 
 
@@ -41,6 +43,8 @@ if __name__ == "__main__":
     parser.add_argument("video_file", type=str)
     parser.add_argument("timings", type=open)
     parser.add_argument("-n", type=int, help="Line number in the timings file")
+    parser.add_argument("--crop", type=str, help="ffmpeg crop arguments")
     options = parser.parse_args()
+
     video_path = os.path.abspath(options.video_file)
-    main(video_path, options.timings, options.n)
+    main(video_path, options.timings, options.n, options.crop)
