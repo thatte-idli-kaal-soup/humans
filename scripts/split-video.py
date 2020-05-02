d0ceac47bc9a714fcabf5ba74e3daf547da968b0
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-05-02 13:07:10 +0530

    Allow processing a specific line of a timings file

--- scripts/split-video.py
+++ scripts/split-video.py
@@ -10,9 +10,11 @@ def to_seconds(timestamp):
     return times[0] * 60 + times[1]
 
 
-def main(video_path, timings):
+def main(video_path, timings, n):
     video_name = os.path.basename(video_path)
     for i, line in enumerate(timings, start=1):
+        if n and i != n:
+            continue
         start, end = line.split("-")
         start_seconds = to_seconds(start)
         end_seconds = to_seconds(end)
@@ -28,7 +30,7 @@ def main(video_path, timings):
                 str(start_seconds),
                 "-t",
                 str(duration),
-                f"part-{i}-{video_name}",
+                f"part-{i:02d}-{video_name}",
             ],
             cwd=os.path.dirname(video_path),
         )
@@ -38,6 +40,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("video_file", type=str)
     parser.add_argument("timings", type=open)
+    parser.add_argument("-n", type=int, help="Line number in the timings file")
     options = parser.parse_args()
     video_path = os.path.abspath(options.video_file)
-    main(video_path, options.timings)
+    main(video_path, options.timings, options.n)
